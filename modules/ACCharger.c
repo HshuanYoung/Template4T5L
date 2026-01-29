61671536be10659727f29540d0857d8d701ec58a
yanghaoshuan <1991317783@qq.com>
2026-01-29 16:00:56 +0800

    更新gitigbore文件，搭建基础框架

--- modules/ACCharger.c
+++ modules/ACCharger.c
@@ -1,15 +1,152 @@
-#ifndef T5FOS_CONFIG_H
-#define T5FOS_CONFIG_H
+#include "ACCharger.h"
 
-#include "t5fos8051.h"
+uint16_t g_charge_state = stateCHARGE_STANDBY;
+ACChargingPara g_ac_charging_para;
+ACChargerInfo g_ac_charger_info;
+NetworkInfo g_network_info;
+LastChargeData g_last_charge_data;
+ConnectPilotPara g_connect_pilot_para;
 
-#define stateCHARGE_STANDBY              0
-#define stateCHARGE_PROCESS              3
-#define stateCHARGE_SETTLED              7
-#define stateCHARGE_UNSETTLED            8
-#define stateCHARGE_BYCARD               9
-#define stateCHARGE_BYAPP                10
-#define stateCHARGE_ABREND_DELAY         11
 
-#define errorCHARGE_JERK                 16
-#define errorC
+/**
+ * 开始充电
+ * 触发方式：屏幕点击开始充电，刷卡触发，app下发
+ */
+static void ACChargeStart(uint8_t startup_type)
+{
+    /*1.充电状态标记*/
+    g_ac_charging_para.is_charge = ON;
+    /*2.记录起始电量*/
+    g_ac_charging_para.startup_watthour = g_ac_charging_para.now_watthour;
+    /*3.充电时长清零*/
+    g_ac_charging_para.charge_time = 0;
+    /*4.记录充电类型*/
+    g_ac_charging_para.startup_type = startup_type;
+    /*5.连接继电器*/
+    ACChargeRelayCtrl(ON);
+    /*6.cp信号pwm使能*/
+    g_connect_pilot_para.pwm_enable = ON;
+    /*7.停止原因清零*/
+    g_ac_charging_para.stop_result = 0;
+}
+
+
+/**
+ * 结束充电
+ * 触发方式：屏幕点击结束充电，刷卡触发，app触发，充电时异常状态触发
+ */
+static void ACChargeStop()
+{
+    if(g_ac_charger_network_info.network_isconnect)
+    {
+       
+    }else
+    {
+        /*1.记录充电信息，卡号，充电时长，电量*/
+        g_last_charge_data.startup_type = g_ac_charging_para.startup_type;
+        memcpy(g_last_charge_data.card_num, g_ac_charging_para.card_num, sizeof(g_last_charge_data.card_num));
+        g_last_charge_data.used_watthour = g_ac_charging_para.now_watthour - g_ac_charging_para.startup_watthour;
+        g_last_charge_data.charge_time = g_ac_charging_para.charge_time;
+        /*2.进入结算页面 */
+        if(g_ac_charging_para.startup_type == CHARGE_STARTUP_BY_CARD)
+        {
+            g_charge_state = stateCHARGE_CARDSETTLE;
+        }else if(g_ac_charging_para.startup_type == CHARGE_STARTUP_BY_SCREEN)
+        {
+            g_charge_state = stateCHARGE_SCREENSETTLE;
+        }
+        g_ac_charging_para.startup_type = 0;
+        /*3.断开继电器*/
+        ACChargeRelayCtrl(OFF);
+        /*4.充电状态标记清零*/
+        g_ac_charging_para.is_charge = OFF;
+    }
+}
+
+
+/**
+ * 恢复自动充电
+ * 触发方式：过压，欠压，电表通讯异常可以自动恢复充电
+ */
+static void ACChargeAutoRecovery()
+{
+    if(g_ac_charging_para.stop_result == errorCHARGE_JERK || g_ac_charging_para.stop_result == errorCHARGE_UNDERVOLTAGE || g_ac_charging_para.stop_result == errorCHARGE_OVERVOLTAGE)
+    {
+        if(!g_ac_charger_info.overvoltage_flag && !g_ac_charger_info.undervoltage_flag && !g_ac_charger_info.jerk_flag)
+        {
+            /*1.记录上一次的充电时间，卡号，启动方式信息*/
+            g_ac_charging_para.charge_time = g_last_charge_data.charge_time;
+            g_ac_charging_para.startup_type = g_last_charge_data.startup_type;
+            memcpy(g_ac_charging_para.card_num, g_last_charge_data.card_num, sizeof(g_ac_charging_para.card_num));
+            /*2.充电状态标记*/
+            g_ac_charging_para.is_charge = ON;
+            /*3.连接继电器*/
+            ACChargeRelayCtrl(ON);
+            /*4.停止原因清零*/
+            g_ac_charging_para.stop_result = 0;
+        }
+    }
+}
+
+
+void ACChargerStateCheck(void)
+{
+    switch(g_charge_state)
+    {
+        case stateCHARGE_STANDBY:
+            if(g_ac_charger_info.jerk_flag)
+            {
+                g_charge_state = errorCHARGE_JERK;
+            }
+            if(g_ac_charger_info.overcurrent_flag)
+            {
+                g_charge_state = errorCHARGE_OVERCURRENT;
+            }
+            if(g_ac_charger_info.overvoltage_flag)
+            {
+                g_charge_state = errorCHARGE_OVERVOLTAGE;
+            }
+            if(g_ac_charger_info.undervoltage_flag)
+            {
+                g_charge_state = errorCHARGE_UNDERVOLTAGE;
+            }
+            if(g_ac_charger_info.leakage_flag)
+            {
+                g_charge_state = errorCHARGE_LEAKAGE;
+            }
+            if(g_ac_charger_info.meter_abnormal_flag)
+            {
+                g_charge_state = errorCHARGE_METERABNORMAL;
+            }
+        break;
+        case stateCHARGE_PROCESS:
+            if(g_ac_charger_info.jerk_flag && !g_ac_charging_para.stop_result)
+            {
+                ACChargeStop();
+                g_ac_charging_para.stop_result = errorCHARGE_JERK;
+            }
+            if(g_ac_charger_info.overcurrent_flag && !g_ac_charging_para.stop_result)
+            {
+                ACChargeStop();
+                g_ac_charging_para.stop_result = errorCHARGE_OVERCURRENT;
+            }
+            if(g_ac_charger_info.overvoltage_flag && !g_ac_charging_para.stop_result)
+            {
+                ACChargeStop();
+                g_ac_charging_para.stop_result = errorCHARGE_OVERVOLTAGE;
+            }
+            if(g_ac_charger_info.undervoltage_flag && !g_ac_charging_para.stop_result)
+            {
+                ACChargeStop();
+                g_ac_charging_para.stop_result = errorCHARGE_UNDERVOLTAGE;
+            }
+            if(g_ac_charger_info.leakage_flag && !g_ac_charging_para.stop_result)
+            {
+                ACChargeStop();
+                g_ac_charging_para.stop_result = errorCHARGE_LEAKAGE;
+            }
+        break;
+
+    }
+
+}
---
+++ modules/ACCharger.h
@@ -0,0 +1,92 @@
+#include "t5los8051.h"
+
+#define stateCHARGE_STANDBY              0
+#define stateCHARGE_PROCESS              3
+#define stateCHARGE_CARDSETTLE           5
+#define stateCHARGE_SCREENSETTLE         7
+#define stateCHARGE_UNSETTLED            8
+#define stateCHARGE_BYCARD               9
+#define stateCHARGE_BYAPP                10
+#define stateCHARGE_ABREND_DELAY         11
+#define stateVEHICLE_CHARGEEND           12
+#define stateCARD_ONLINEWAIT            13
+#define stateCARD_LOCK                   14
+#define stateCARD_LOWMONEY               15
+
+#define errorCHARGE_JERK                 16
+#define errorCHARGE_OVERCURRENT          17
+#define errorCHARGE_OVERVOLTAGE         18
+#define errorCHARGE_UNDERVOLTAGE         19
+#define errorCHARGE_LEAKAGE              20
+#define errorCHARGE_METERABNORMAL        21
+#define errorVEHICLE_GUNCONNECT          22
+#define errorVEHICLE_ABNORMAL            23
+#define errorCARD_NOBINDING              24
+#define errorCARD_STARTUPDIFF            25
+
+
+#define ON    1
+#define OFF   0
+#define CHARGE_STARTUP_BY_CARD            1
+#define CHARGE_STARTUP_BY_APP             2
+#define CHARGE_STARTUP_BY_SCREEN          3
+
+
+/**
+ * 充电参数结构体
+ */
+typedef struct
+{
+    uint16_t voltage;
+    uint16_t current;
+    uint16_t power;
+    uint16_t stop_result;
+    uint8_t startup_type;
+    uint8_t card_num[32];
+    float startup_watthour;
+    float now_watthour;
+    uint32_t charge_time;
+    uint8_t is_charge;
+
+}ACChargingPara;
+
+typedef struct 
+{
+    uint8_t pwm_enable;
+    uint16_t pwm_duty;
+}ConnectPilotPara;
+
+
+/**
+ * 测试标志位结构体
+ */
+typedef struct
+{
+    uint8_t jerk_flag;
+    uint8_t overcurrent_flag;
+    uint8_t overvoltage_flag;
+    uint8_t undervoltage_flag;
+    uint8_t leakage_flag;
+    uint8_t meter_abnormal_flag;
+}ACChargerInfo;
+
+typedef struct 
+{
+    uint8_t network_isconnect;
+}NetworkInfo;
+
+typedef struct 
+{
+    uint8_t startup_type;
+    uint8_t card_num[32];
+    float used_watthour;
+    uint32_t charge_time;
+}LastChargeData;
+
+
+extern ACChargingPara g_ac_charging_para;
+extern ACChargerInfo g_ac_charger_info;
+extern NetworkInfo g_network_info;
+extern LastChargeData g_last_charge_data;
+extern ConnectPilotPara g_connect_pilot_para;
+extern uint16_t g_charge_state;
