8ad6f15798fb09aa9c6e0ccb2f7c29d268bf2a07
yanghaoshuan <1991317783@qq.com>
2026-01-29 10:26:05 +0800

    zancun

---
+++ modules/ACCharger.c
@@ -0,0 +1,15 @@
+#ifndef T5FOS_CONFIG_H
+#define T5FOS_CONFIG_H
+
+#include "t5fos8051.h"
+
+#define stateCHARGE_STANDBY              0
+#define stateCHARGE_PROCESS              3
+#define stateCHARGE_SETTLED              7
+#define stateCHARGE_UNSETTLED            8
+#define stateCHARGE_BYCARD               9
+#define stateCHARGE_BYAPP                10
+#define stateCHARGE_ABREND_DELAY         11
+
+#define errorCHARGE_JERK                 16
+#define errorC
